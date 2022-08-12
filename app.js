0c717fbc6a2bbe48be1389f1df96052fee4e37ee
Heikki Mattila <matthe22@RPKK.FI>
2022-08-12 09:08:25 +0300

    input ja button lisätty

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 let a = 12;
 console.log(a);
 let tulos = a*a;
-console.log("Tulos on:  " + tulos);
+console.log("Tulos on: " + tulos);
 
-
-
+// funktio hakee luvun html-sivulta ja kirjoittaa tuloksen html-sivulle
+function omaFunktio() {
+    let x = document.getElementById("luku").value;
+    document.getElementById("tulos").innerHTML = x * x;
+  }
 
